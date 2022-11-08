ffa9812506a5d5682e5b7b1dc04478b1f3e212f6
mmitkevich <mmitkevich@rencap.com>
2022-11-08 10:58:28 -0500

    run.sh

--- run.sh
+++ run.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 ROQ_DIR=$(realpath $(dirname $0)/../roq-rencap)
 UMM_DIR=$(realpath $(dirname $0)/../umm)
+STRATEGY=${STRATEGY:-AlphaMakerDeribit}
 cd $ROQ_DIR
 ROQ_v=2 ./bin/roq-mmaker-app \
     --name mmaker \
     --config_file $UMM_DIR/src/umm/model/alpha_maker/alpha_maker.toml \
-    --strategy AlphaMakerDeribit \
+    --strategy $STRATEGY \
     $ROQ_DIR/run/deribit-demo.sock \
     $ROQ_DIR/run/ftx-demo.sock
 
